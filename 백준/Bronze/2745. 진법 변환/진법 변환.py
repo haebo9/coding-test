1edb80ab6abafac87bf63688aa6041de9ff1c22c
haebo <98heabo@gmail.com>
2026-01-23 16:10:53 +0900

    [Bronze II] Title: 진법 변환, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,3 +1,21 @@
+# 알파벳 대문자-> 숫자 리스트
+def alp_to_numlist(alp):
+    # A가 10, Z가 35
+    return list(map(lambda x: int(x) if x.isdigit() else ord(x)-55, list(alp)))
+
+# 진법 변환 B -> 10 진법
+def B_to_10(alp:list, B:int) -> int: 
+    result = 0
+    for i, a in enumerate(alp[::-1]):
+        result += a*(B**i)
+    return result
+
+import sys
+input = sys.stdin.readline
 N, B = input().split()
 
-print(int(N, int(B)))
+B = int(B)
+alp = alp_to_numlist(N)
+result = B_to_10(alp, B)
+
+print(result)
