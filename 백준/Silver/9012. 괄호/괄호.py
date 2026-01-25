cac06378c2a6b303a0282ccaf798a66844ac0b71
haebo <98heabo@gmail.com>
2026-01-26 01:32:56 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,25 @@
+# import sys
+# input = sys.stdin.readline
+
+def check_vps(data:str)->bool:
+    stack = []
+    for i in data:
+        if i == '(':
+            stack.append('(')
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+    else:
+        if not stack: # 비어있으면
+            return True
+        else: # 안비어있으면
+            return False
+
+for _ in range(int(input())):
+    user = input()
+    if check_vps(user):
+        print('YES')
+    else:
+        print('NO')
