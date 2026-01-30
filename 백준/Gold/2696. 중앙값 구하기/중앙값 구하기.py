c8a547c6d9b6a24ba25360a5fc445705a524e538
haebo <98heabo@gmail.com>
2026-01-31 00:52:12 +0900

    [Gold II] Title: 중앙값 구하기, Time: 56 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -1,29 +1,50 @@
 import sys
 input = sys.stdin.readline
-# t = int(input())
 
-# 홀수 번째 읽었을 때 중앙값 출력 
-    # 수열의 크가 9 
-    # 출력 번호 : 1 3 5 7 9 
-    # 인덱스 값 : 0 2 4 6 8
-    # 중앙 번호 : 1 2 3 4 5
-    # 중앙인덱스 : 0 1 2 3 4
+import heapq 
 
-def middle(m:int, nums:list): 
-    check = list(map(lambda x: x, range(0, m+1, 2)))
-    mid = [sorted(nums[:i+1])[i//2] for i in check]
+def middle(c, up_mid, dn_mid):
+    # 최소힙이 개수가 적으면 up에 기본으로 들어감
+    if len(up_mid) <= len(dn_mid):
+        heapq.heappush(up_mid, c)
     
+    # 최대힙이 개수가 적으면 dn에 기본으로 들어감
+    elif len(up_mid) > len(dn_mid):
+        heapq.heappush(dn_mid, -c)
+        
+    # 최대힙의 루트 값이 최소힙의 루트보다 커지면 교체 
+    while up_mid and dn_mid and up_mid[0] < -dn_mid[0]: 
+        top = heapq.heappop(up_mid)
+        down = -heapq.heappop(dn_mid)
+        heapq.heappush(dn_mid, -top)
+        heapq.heappush(up_mid, down)
+
+    return up_mid[0]
+
+
+def check(m, nums): 
+    up_mid = [] # 중앙값 보다 큰 수들 : 최소힙
+    dn_mid = [] # 중앙값 보다 작은 수들 : 최대힙
+
+    # 값을 하나씩 넣어주다가 홀수 번호에서 중앙값 저장
+    mid = []
+    for i, c in enumerate(nums):
+        if i % 2 ==0: 
+            mid.append(middle(c, up_mid, dn_mid))  
+        else: 
+            middle(c, up_mid, dn_mid)
+
+    # 10개씩 끊어서 출력
     l = len(mid) ; print(l)
     start, end = 0, 10
     for _ in range(l//10 + 1):
         print(*mid[start:min(end, l)]) 
         start, end = start + 10, end + 10
-        
 
-
+# 10개씩 끊어서 입력 / 결과 반환
 for _ in range(int(input())):
     m = int(input())
     nums = []
     for _ in range(m//10 +1):
         nums.extend(map(int, input().strip().split()))
-    middle(m, nums)
+    check(m, nums)
