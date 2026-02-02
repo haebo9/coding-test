12a9d8c426b43316644acc4ee793b3d87a49f5a0
haebo <98heabo@gmail.com>
2026-02-02 16:44:45 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 496 ms, Memory: 160988 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -3,19 +3,34 @@ input = sys.stdin.readline
 
 s = input().strip()
 q = int(input().strip())
-
+set_s = set(s) 
 n = len(s)
 
-alpha_tf = {i: [0]*n for i in s}
-for i,x in enumerate(s): 
-    alpha_tf[x][i] = 1
+# 1. 모든 알파벳에 대해 미리 누적 합 리스트를 생성 (n+1 크기 권장)
+alpha_tf = {chr(c): [0] * (n + 1) for c in range(ord('a'), ord('z') + 1)}
+
+# 2. 문자열을 딱 한 번만 순회 (O(N))
+for i, char in enumerate(s):
+    # 우선 모든 알파벳의 이전 값을 현재 칸으로 복사
+    for char_key in set_s:
+        alpha_tf[char_key][i + 1] = alpha_tf[char_key][i]
+    
+    # 현재 위치의 문자에 대해서만 1을 더함
+    alpha_tf[char][i + 1] += 1
 
 # print(alpha_tf)
 
+start = 'a'
+target = alpha_tf[start]
 for _ in range(q):
     a, i, j = input().strip().split()
-    if a in alpha_tf.keys(): 
-        result = sum(alpha_tf[a][int(i):int(j)+1])
-        print(result)
+    if a in set_s: 
+        if  a != start: 
+            start = f'{a}'
+            target = alpha_tf[start]
+        target = alpha_tf[a]
+        i, j = int(i)+1, int(j)+1
+        print(target[j] - target[i-1])
+
     else: 
         print(0)
