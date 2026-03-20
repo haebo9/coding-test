52fc09f6f288ba2e5e6be998c0016b32531efbb5
haebo <98heabo@gmail.com>
2026-03-20 22:27:05 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 72 ms, Memory: 45488 KB -BaekjoonHub

---
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+N, game = input().split()
+names = set([input().strip() for _ in range(int(N))])
+
+games = {'Y':2, 'F':3, 'O':4}
+print(len(names)//(games[game]-1))
