35a212dbe8d31da8fc1ba681a5959826fd5791ef
haebo <98heabo@gmail.com>
2026-03-21 00:01:05 +0900

    [Silver III] Title: 타노스, Time: 56 ms, Memory: 34908 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,5 +1,24 @@
 from collections import Counter
 import sys
 input = sys.stdin.readline
-c = Counter(input().strip())
-print('0'*(c['0']//2) + '1'*(c['1']//2))
+
+# 1은 최대한 앞의 것을 지우고, 0은 최대한 뒤의 것을 지우면 될듯
+
+s = input().strip()
+
+# 지워야 하는 개수를 확인
+c = Counter(s)
+t0 = c['0']//2
+t1 = c['1']//2
+
+result = list(s)
+
+for i in range(c['0'] + c['1']): 
+    if s[i] == '1' and t1: 
+        result[i] = ''
+        t1 -= 1
+    if s[-1-i] == '0' and t0: 
+        result[-1-i] = ''
+        t0 -= 1
+
+print(''.join(result))
