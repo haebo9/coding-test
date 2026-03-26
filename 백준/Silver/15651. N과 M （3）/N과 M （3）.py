bd7f3a869a52749bef2e3d69bbe4da1c97efe473
haebo <98heabo@gmail.com>
2026-03-27 01:43:47 +0900

    [Silver III] Title: N과 M (3), Time: 1888 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,21 +1,14 @@
-def backtrack(): 
-    if len(arr) >= m: 
-        print(*arr)
-        return 
-
-    for i in range(1, n+1): 
-        visited[i] = True
-        arr.append(i)
-
-        backtrack()
-
-        arr.pop()
-        visited[i] = False
-
 import sys
 input = sys.stdin.readline 
 
-n, m = map(int, input().strip().split())
-arr = []
-visited = [False]*(n+1)
-backtrack()
+def backtrack(result): 
+    if len(result)==m: 
+        print(*result)
+        return 
+
+    for i in range(1, n+1): 
+        backtrack(result+[i]) 
+
+n, m = map(int, input().split())
+result = []
+backtrack(result)
