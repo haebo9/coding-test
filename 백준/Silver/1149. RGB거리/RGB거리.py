fdb712daab0bde8ab8f87255e007f12b3fde768a
haebo <98heabo@gmail.com>
2026-03-27 01:29:50 +0900

    [Silver I] Title: RGB거리, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,17 +1,13 @@
-# DP 
-# 1번 집부터 R,G,B를 칠했을때의 최솟값을 각각 갱신하면서 진행
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline 
 
-n = int(input()) 
-house = [list(map(int, input().strip().split())) for _ in range(n)]
+n = int(input())
+arr = [tuple(map(int, input().strip().split())) for _ in range(n)]
+dp = [0,0,0]
 
-for i in range(1, n): 
-    # i번째 집을 r로 칠하는 최솟비용 = i-1번째 집이 g 또는 b일때의 최소 비용과 r로 칠하는 비용의 합
-    house[i][0] += min(house[i-1][1], house[i-1][2])
-    house[i][1] += min(house[i-1][0], house[i-1][2])
-    house[i][2] += min(house[i-1][0], house[i-1][1])
+# arr(집)을 하나씩 순회하며 dp 최솟값 갱신 
+for i in range(n): 
+    r,g,b = arr[i]
+    dp[0],dp[1],dp[2] = r + min(dp[1], dp[2]), g + min(dp[0], dp[2]), b + min(dp[0], dp[1])
 
-# r,g,b의 최소비용 중 최소인 값을 선택
-result = min(house[-1])
-print(result)
+print(min(dp))
