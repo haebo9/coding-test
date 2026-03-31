745a624f7fcd5afc5c0c96e9db152197bbbf6c59
haebo <98heabo@gmail.com>
2026-03-31 20:35:26 +0900

    [Silver III] Title: 가희와 키워드, Time: 768 ms, Memory: 54944 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.py
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.py
@@ -3,7 +3,6 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 keyword = set()
-use = set() 
 
 for _ in range(n): 
     keyword.add(input().strip())    
@@ -11,5 +10,4 @@ for _ in range(n):
 for _ in range(m): 
     for k in input().strip().split(','):
         keyword.discard(k)
-        use.add(k)
     print(len(keyword))
