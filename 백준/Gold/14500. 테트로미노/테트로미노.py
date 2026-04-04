f7c52936bab0d6f3979a528a67dcd093de5cff3d
haebo <98heabo@gmail.com>
2026-04-04 21:25:16 +0900

    [Gold IV] Title: 테트로미노, Time: 3852 ms, Memory: 39912 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,5 +1,5 @@
 import sys
-input = sys.stdin.readline 
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 board = [list(map(int, input().strip().split())) for _ in range(n)]
@@ -8,63 +8,58 @@ board = [list(map(int, input().strip().split())) for _ in range(n)]
 # board = [[1, 2, 3, 4, 5], [5, 4, 3, 2, 1], [2, 3, 4, 5, 6], [6, 5, 4, 3, 2], [1, 2, 1, 2, 1]]
 # print(board)
 
-def solution(n, m, board): 
-# 1. 직선형 (2가지)
-    type1 = [
-        [(0, 0), (0, 1), (0, 2), (0, 3)], 
-        [(0, 0), (1, 0), (2, 0), (3, 0)]
-    ] 
+def solution(n, m, board):
+    result = 0
 
-    # 2. 정사각형 (1가지)
-    type2 = [
-        [(0, 0), (0, 1), (1, 0), (1, 1)]
-    ] 
+    def dfs(r, c, total, cnt): 
+        nonlocal result, visited
 
-    # 3. 니은(ㄴ)형 (8가지: 회전 4 + 대칭 회전 4)
-    type3 = [
-        [(0,0), (1,0), (2,0), (2,1)], 
-        [(0,0), (1,0), (2,0), (2,-1)],
-        [(0,0), (0,1), (0,2), (1,2)],
-        [(0,0), (0,1), (0,2), (1,0)],
-        [(0,0), (0,1), (1,1), (2,1)],
-        [(0,0), (0,1), (1,0), (2,0)],
-        [(0,0), (1,0), (1,1), (1,2)],
-        [(0,1), (1,1), (1,0), (1,-1)]
-    ]
+        if cnt == 4: 
+            if total > result: 
+                result = total
+            return 
 
-    # 4. 시그모이드(번개)형 (4가지: 회전 2 + 대칭 회전 2)
-    type4 = [
-        [(0, 0), (1, 0), (1, 1), (2, 1)], 
-        [(0, 0), (1, 0), (1, -1), (2, -1)],
-        [(0, 0), (0, 1), (-1, 1), (-1, 2)], 
-        [(0, 0), (0, 1), (1, 1), (1, 2)]
-    ]
+        for dr, dc in [(0,-1), (0, 1), (-1, 0), (1, 0)]: # 좌 우 상 하
+            nr, nc = r+dr, c+dc
+            if 0 <= nr < n and 0 <= nc < m and not visited[nr][nc]: 
+                visited[nr][nc] = 1
+                dfs(nr, nc, total+board[nr][nc], cnt+1)
+                visited[nr][nc] = 0 # 백트랙킹 
 
-    # 5. 산(ㅜ) 형 (4가지)
-    type5 = [
-        [(0, 0), (0, -1), (0, 1), (1, 0)],  # ㅜ
-        [(0, 0), (0, -1), (0, 1), (-1, 0)], # ㅗ
-        [(0, 0), (-1, 0), (1, 0), (0, 1)],  # ㅏ
-        [(0, 0), (-1, 0), (1, 0), (0, -1)]  # ㅓ
-    ]
+    def check(n, m, board, r, c): 
+        nonlocal result
+        # DFS로 알수 없는 값 : 산(ㅜ) 형 (4가지)
+        check = [
+            [(0, 0), (0, -1), (0, 1), (1, 0)],  # ㅜ
+            [(0, 0), (0, -1), (0, 1), (-1, 0)], # ㅗ
+            [(0, 0), (-1, 0), (1, 0), (0, 1)],  # ㅏ
+            [(0, 0), (-1, 0), (1, 0), (0, -1)]  # ㅓ
+        ]
 
-    answer = 0
-    # 모든 유형에 대해 수의 최댓값 계산 
-    for tp in [type1,type2,type3,type4,type5]:
+        # 외부에서 산 형 값 직접 계산
+        for ck in check: 
+            total = 0
+            for dr, dc in ck: 
+                nr, nc = r+dr, c+dc
+                if 0 <= nr < n and 0 <= nc < m: 
+                    total+= board[nr][nc]
+                else: 
+                    break
+            if total > result: 
+                result = total
 
-        for r in range(n): 
-            for c in range(m): 
-                for curr_tp in tp: 
-                    temp = 0
-                    for dr, dc in curr_tp: 
-                        nr, nc = r+dr, c+dc
-                        if 0<= nr < n and 0<= nc < m: 
-                            temp += board[nr][nc]
-                        else: 
-                            break 
-                    else: 
-                        if temp > answer: 
-                            answer = temp
-    print(answer)
-                                
-solution(n, m, board)
+    visited = [[0]*m for _ in range(n)]
+    for r in range(n): 
+        for c in range(m): 
+            # 내부에서 result 갱신 (DFS 로직 탐색) / 현재위치의 값 반영
+            visited[r][c] = 1 # 시작 값 방문 처리
+            dfs(r, c, total=board[r][c], cnt=1) 
+            visited[r][c] = 0 # 백트랙킹 (방문 회수)
+
+            # 내부에서 result 갱신 (ㅗ 모양 직접 탐색)
+            check(n, m, board, r, c)
+    
+    return result
+
+result = solution(n, m, board) 
+print(result)
