3333e66bb45dd15c3331f87daef146a975ccb691
haebo <98heabo@gmail.com>
2026-04-09 23:39:19 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 220 ms, Memory: 39364 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,35 @@
+import heapq
+import sys
+input = sys.stdin.readline 
+
+def bfs(n, k): 
+    """
+    전략: 수빈이의 위치에서 즉시 가능한 모든 순간이동을 수행하고, 1초 후 위치 이동을 수행한다.
+    
+    알고리즘 : 다익스트라 (heapq) - 특정 위치에서의 최단거리 보장 
+    가중치가 0과 1로 이루어진 그래프 탐색
+    """
+    q = [] 
+    heapq.heappush(q, (1, n)) # (w, n): w는 진행된 시간, n은 현재 위치
+    
+    visited = [0]*100001
+
+    while q: 
+        w, x= heapq.heappop(q) # x에서의 최단시간
+
+        if not visited[x] or w < visited[x]:  
+            visited[x] = w 
+
+            if x == k: # 목적지 도착 
+                return w-1 # 시작 시간 1 -> 0으로 값 보정 
+            
+            for mv in [x*2]: 
+                if 0<= mv <= 100000:
+                    heapq.heappush(q, (w, mv))
+
+            for mv in [x-1, x+1]: 
+                if 0<= mv <= 100000:
+                    heapq.heappush(q, (w+1, mv))
+
+n, k = map(int, input().split())
+print(bfs(n, k))
