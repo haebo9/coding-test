6e7b22f490faa85a4a148aad59f6fa9721916670
haebo <98heabo@gmail.com>
2026-04-14 18:09:57 +0900

    [Gold V] Title: 전깃줄, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,8 +1,5 @@
-k = int(input())
-arr = [list(map(int, input().strip().split())) for _ in range(k)]
-# k = 8
-# arr = [[1, 8], [3, 9], [2, 2], [4, 1], [6, 4], [10, 10], [9, 7], [7, 6]]
-# print(arr) 
+import sys
+input = sys.stdin.readline 
 
 def solution(k, arr): 
     "1번부터 끝번호 까지 교차하지 않고 연결할 수 있는 전기줄의 개수를 저장한다.(dp)"
@@ -18,16 +15,19 @@ def solution(k, arr):
         # 인덱스는 출발 위치의 번호, 인덱스에서의 값은 연결된 전깃줄의 번호 
         lines[i] = c
         dp[i] = 1 # 전깃줄 연결된 곳은 이미 1개 연결 
-    # print(dp)
-    # print(lines)
-    # print()
 
     # 각 위치에서의 연결 가능한 전봇대 줄 수 저장 
     for i, c in arr: 
-        for j in range(1, i): # 1~i-1
-            if lines[i] > lines[j] and lines[j]: # 현재 값보다 작은 값 중에서
+        for j, _ in arr: # 1~i-1
+            # 비교 범위 지정 
+            if j > i: 
+                break
+            # 현재 값보다 작은 값 중에서 최대 + 1
+            if c > lines[j] and lines[j]: 
                 dp[i] = max(dp[j]+1, dp[i])
-        # print(dp)
-    return k - max(dp)
 
+    return k - max(dp) # 제거해야 하는 전기줄 수 계산 
+
+k = int(input())
+arr = [list(map(int, input().strip().split())) for _ in range(k)]
 print(solution(k, arr))
