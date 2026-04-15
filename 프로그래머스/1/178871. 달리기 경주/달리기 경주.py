b16e357f9dcd1a63007f8e33ca30ceff258c9e57
haebo <98heabo@gmail.com>
2026-04-15 20:45:59 +0900

    [level 1] Title: 달리기 경주, Time: 648.73 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,14 +1,14 @@
 def solution(players, callings):
+    # 각 선수의 시작 위치를 담은 딕셔너리 
     numbers = {p:i for i, p in enumerate(players)}
     
     for c in callings: 
         # 이름 불린 선수의 위치
         idx = numbers[c]
+        prev = players[idx-1]
         
         # 앞선수와 위치 교환 
-        prev = players[idx-1]
-        players[idx-1] = players[idx]
-        players[idx] = prev
+        players[idx-1], players[idx] = players[idx], players[idx-1]
         
         # 딕셔너리의 위치값 수정 
         numbers[prev] += 1
