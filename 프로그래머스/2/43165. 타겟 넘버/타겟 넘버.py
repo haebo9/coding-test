f8a1aaaf9b3e8cd56f5e19b68f02fc2a64938c03
haebo <98heabo@gmail.com>
2026-04-28 23:42:06 +0900

    [level 2] Title: 타겟 넘버, Time: 237.18 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,28 @@
+from collections import deque 
+
 def solution(numbers, target):
     n = len(numbers)
-    answer = 0
-    def backtrack(idx, total): 
-        nonlocal n, answer
-        if idx == n: 
-            if total == target: 
-                answer += 1
-            return 
+    
+    def num_bfs(numbers): 
+        # 누적합에 사용될 배열
+        num = deque(numbers)
+        start = num.popleft()
+        
+        # 시작 값을 deuqe에 넣어줌 
+        q = deque() 
+        q.append(start)
+        q.append(-start)
+        
+        # bfs 탐색 시작 : 부분 누적 합에 새로운 값을 추가 
+        while num:
+            new = num.popleft()
             
-        for num in (numbers[idx], -numbers[idx]): 
-            backtrack(idx+1, total+num)
+            for _ in range(len(q)): 
+                local_sum = q.popleft()
+                q.append(local_sum + new)
+                q.append(local_sum - new)
+        
+        # 최종 누적 합에서 찾는 숫자의 개수 세기
+        return q.count(target)
     
-    backtrack(idx=0, total=0)
-    
-    return answer
+    return num_bfs(numbers)
